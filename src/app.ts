956f928898ad365c86c7f139d73d71de488ffd00
khalil-chermiti <khalil666chermiti@gmail.com>
2022-12-20 19:43:25 +0100

    fix(app.ts): add type definitions for error hander

--- src/app.ts
+++ src/app.ts
@@ -21,8 +21,10 @@ app.get('/', (req, res) => {
 
 app.use('/api/v1/auth', authRouter);
 
-app.use((err: ErrorHandler, req: Request, res: Response) => {
-  return res.status(err.statusCode).send({ error: err.message });
-});
+app.use(
+  (err: ErrorHandler, req: Request, res: Response, next: NextFunction) => {
+    return res.status(err.statusCode).send({ error: err.message });
+  }
+);
 
 export default app;
