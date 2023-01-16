6c069e1750ac1b75c625b4cbb58c7b944642635a
khalil-chermiti <khalil666chermiti@gmail.com>
2023-01-16 09:02:47 +0100

    feat(auth): prevent user from signin/singup while still logged in

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -4,17 +4,18 @@ import { signup } from '../controllers/singupController';
 import { signin } from '../controllers/signinController';
 import { resetPassword } from '../controllers/resetPassword';
 import { getResetLink } from '../controllers/resetLinkController';
+import { protectAuth } from '../middlewares/protectAuth';
 
 const authRouter = express.Router();
 
-authRouter.route('/client/signup').post(signup);
-authRouter.route('/client/signin').post(signin);
+authRouter.route('/client/signup').post(protectAuth, signup);
+authRouter.route('/client/signin').post(protectAuth, signin);
 authRouter.route('/client/link').post(getResetLink);
 authRouter.route('/client/reset').get(resetPassword);
 authRouter.route('/client/signout').post(signout);
 
-authRouter.route('/employee/signup').post(signup);
-authRouter.route('/employee/signin').post(signin);
+authRouter.route('/employee/signup').post(protectAuth, signup);
+authRouter.route('/employee/signin').post(protectAuth, signin);
 authRouter.route('/employee/link').post(getResetLink);
 authRouter.route('/employee/reset').get(resetPassword);
 authRouter.route('/employee/signout').post(signout);
